e7ea40c8b453437a33cc30e81e687beda980113e
WatarizZz-tests <120670228+WatarizZz-tests@users.noreply.github.com>
2024-08-12 12:08:21 +0100

    Update Video.jsx

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './videostyle.css';
 import videoSrc from '../../assets/videomainpage.mp4';
-
+import fallback from '../../assets/videofallback.png';
 const Video = () => {
   return (
     <div className='video-container'>
-      <video src={videoSrc} autoPlay loop muted playsinline type="video/mp4" className='background-video'></video>
+      <video src={videoSrc} autoPlay loop muted playsinline type="video/mp4" poster={fallback} className='background-video'></video>
       <div className='overlay'></div>
       <div className='content'>
         <h1>Crafted with Love, Worn with Confidence.</h1>
